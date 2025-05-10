b3161e12c2a4e6d6dbd202db05003fa1929d3e48
luannhat <luannhat44@gmail.com>
2025-05-10 11:06:41 +0700

    upload danhmuc

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('welcome');
 })->name('home');
+
+
+//route admin
+Route::prefix('/')->middleware('admin.login')->group(function(){
+
+  Route::get('/admin/danhmuc', [DanhmucController::class, 'index'])->name('danhmuc.index');
+  Route::get('/admin/danhmuc/create', [DanhmucController:: class, 'create'])->name('danhmuc.create');
+  Route::post('/admin/danhmuc', [DanhmucController::class, 'store'])->name('danhmuc.store');
+  Route::get('/admin/danhmuc/edit/{danhmuc}', [DanhmucController::class, 'edit'])->name('danhmuc.edit');
+  Route::put('/admin/danhmuc/update/{danhmuc}', [DanhmucController::class, 'update'])->name('danhmuc.update');
+  Route::delete('/admin/danhmuc/{danhmuc}/destroy', [DanhmucController::class, 'destroy'])->name('danhmuc.destroy');
+});
