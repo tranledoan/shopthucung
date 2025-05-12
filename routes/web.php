86c91234c11c5b7175e4597a279c98b6e527d8d2
DESKTOP-Q6NJCB3\Win <tranledoan28022000@gmail.com>
2025-05-12 18:52:44 +0700

    đăng ký và đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
+
 Route::get('/', function () {
-  return view('welcome');
+  return view('layout');
 })->name('home');
+
+Route::get('/login-khachhang', [\App\Http\Controllers\Auth\LoginKhachHangController::class, 'showLoginForm'])->name('login.khachhang');
+Route::post('/login-khachhang', [\App\Http\Controllers\Auth\LoginKhachHangController::class, 'login']);
+Route::post('/logout-khachhang', [\App\Http\Controllers\Auth\LoginKhachHangController::class, 'logout'])->name('logout.khachhang');
+
+// router login , register ,logout
+Route::get('/login', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
