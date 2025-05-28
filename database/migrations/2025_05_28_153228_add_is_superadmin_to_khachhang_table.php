7d8530302349ea122b50ef40626e7415c1166e8f
hieuc <hieuchung428@gmail.com>
2025-05-28 22:56:22 +0700

    update xoa sua user

---
+++ database/migrations/2025_05_28_153228_add_is_superadmin_to_khachhang_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsSuperadminToKhachhangTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('khachhang', function (Blueprint $table) {
+            $table->boolean('is_superadmin')->default(false);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('khachhang', function (Blueprint $table) {
+            $table->dropColumn('is_superadmin');
+        });
+    }
+}
